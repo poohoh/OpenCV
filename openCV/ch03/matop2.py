a396ae69af3bf76e38774a8dbb1869b23fbeecc5
poohoh <kjh980328@gmail.com>
2023-03-30 18:10:41 +0900

    ch03 matop2.py

---
+++ openCV/ch03/matop2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+
+def func2():
+    img1 = np.empty((480, 640), np.uint8)       # grayscale
+    img2 = np.zeros((480, 640, 3), np.uint8)    # color
+    img3 = np.ones((480, 640), np.int32)        # 1's matrix
+    img4 = np.full((480, 640), 0, np.float32)   # 0.0 matrix
+
+    mat1 = np.array([[11, 12, 13, 14],
+                     [21, 22, 23, 24],
+                     [31, 32, 33, 34]]).astype(np.uint8)
+
+    mat1[0, 1] = 100  # x=1, y=0의 값
+    mat1[2, :] = 200
+
+    print(mat1)
+
+
+if __name__ == '__main__':
+    func2()

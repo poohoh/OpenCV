7daf4c88e241eff4f38038664d04d8f83ea460e6
poohoh <kjh980328@gmail.com>
2023-03-30 18:10:27 +0900

    ch03 matop1.py

---
+++ openCV/ch03/matop1.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+
+def func1():
+    img1 = cv2.imread('cat.bmp', cv2.IMREAD_GRAYSCALE)
+
+    if img1 is None:
+        print('Image load failed!')
+        return
+
+    print('type(img1):', type(img1))
+    print('img1.shape:', img1.shape)
+
+    if len(img1.shape) == 2:
+        print('img1 is a grayscale image')
+    elif len(img1.shape) == 3:
+        print('img1 is a truecolor image')
+
+    cv2.imshow('img1', img1)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    func1()

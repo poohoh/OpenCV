d27434c18ce9b8982d7eb40063d699cc7800227e
poohoh <kjh980328@gmail.com>
2023-04-02 00:29:48 +0900

    ch05 grayscale3

---
+++ openCV/ch05/5.1.1.grayscale3.py
@@ -0,0 +1,10 @@
+import cv2
+
+img3 = cv2.imread('lenna.bmp', cv2.IMREAD_COLOR)
+img4 = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('img3', img3)
+cv2.imshow('img4', img4)
+
+cv2.waitKey()
+cv2.destroyAllWindows()

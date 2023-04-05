9de8d712bcd7bf2bbf9fa1a69f3de160875a1103
poohoh <kjh980328@gmail.com>
2023-04-05 20:42:37 +0900

    ch05 brightness1 - 영상 밝기 조절

---
+++ openCV/ch05/5.1.2.brightness1.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+
+def brightness1():
+    src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+
+    if src is None:
+        print('Image load failed!')
+        return
+
+    dst = cv2.add(src, 100)
+    # dst = cv2.add(src, -100)
+
+    cv2.imshow('src', src)
+    cv2.imshow('dst', dst)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    brightness1()

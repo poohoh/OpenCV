33bcc4a8075778dafc8518bc94a831cde69d52bb
poohoh <kjh980328@gmail.com>
2023-04-05 20:44:10 +0900

    ch05 brightness3 - 포화 연산

---
+++ openCV/ch05/5.1.2.brightness3.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+
+
+def saturated(value):
+    if value > 255:
+        value = 255
+    elif value < 0:
+        value = 0
+
+    return value
+
+
+def brightness3():
+    src = cv2.imread('lenna.bmp', cv2.IMREAD_GRAYSCALE)
+
+    if src is None:
+        print("Image load failed!")
+        return
+
+    dst = np.empty(src.shape, src.dtype)
+
+    for y in range(src.shape[0]):
+        for x in range(src.shape[1]):
+            dst[y, x] = saturated(src[y, x] + 100)
+
+    cv2.imshow('src', src)
+    cv2.imshow('dst', dst)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    brightness3()
